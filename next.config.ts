5346b31345d617af470cce9a6d5c9d534457e6d3
stoffis-git <your-email@example.com>
2025-12-19 08:35:33 +0700

    fix: add image configuration for external domains

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'get-better.co',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
